4b6e2bb619a8f95b18e31e48e8c6fb8cfb8cd21d
Ben Laurie <ben@algroup.co.uk>
2000-06-27 20:59:07 +0000

    Add licence, fix bug, update theory.

--- java/uk/co/aldigital/ben/lucre/DoubleCoinRequest.java
+++ java/uk/co/aldigital/ben/lucre/DoubleCoinRequest.java
@@ -1,3 +1,50 @@
+/* ====================================================================
+ * Copyright (c) 1999, 2000 Ben Laurie.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by Ben Laurie
+ *    for use in the Lucre project."
+ *
+ * 4. The name "Lucre" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission.
+ *
+ * 5. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Ben Laurie
+ *    for use in the Lucre project."
+ *
+ * THIS SOFTWARE IS PROVIDED BY BEN LAURIE ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL BEN LAURIE OR
+ * HIS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * For more information on Lucre see http://anoncvs.aldigital.co.uk/lucre/.
+ *
+ */
+
 package uk.co.aldigital.ben.lucre;
 
 import java.io.BufferedReader;
@@ -26,9 +73,23 @@ class DoubleCoinRequest extends PublicCoinRequest {
 	    }
 
 	// choose b_y
-	m_biBlindingFactorY=new BigInteger(PublicBank.BLINDING_LENGTH*8,
-					  Util.randomGenerator());
-	Util.dumpNumber("by=       ",m_biBlindingFactorY);
+	boolean ok=false;
+	while(!ok)
+	    {
+	    m_biBlindingFactorY=new BigInteger(PublicBank.BLINDING_LENGTH*8,
+					       Util.randomGenerator());
+	    Util.dumpNumber("by=       ",m_biBlindingFactorY);
+	    BigInteger p2=bank.getExponentGroupOrder();
+	    ok=true;
+
+	    // I expect there's a cheaper way to do this.
+	    // Alternately, store the inverse, since that's what we need later
+	    // anyway.
+	    try
+		{ BigInteger byinv=m_biBlindingFactorY.modInverse(p2); }
+	    catch(ArithmeticException e)
+		{ System.out.println("Failed: "+e+", try again!"); ok=false; }
+	    }
 
 	// choose b_g
 	m_biBlindingFactorG=new BigInteger(PublicBank.BLINDING_LENGTH*8,
