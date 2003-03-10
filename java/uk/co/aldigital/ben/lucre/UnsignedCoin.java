a7c03446a75b03e247eb505633c81392881742bb
Ben Laurie <ben@algroup.co.uk>
2003-03-10 10:17:38 +0000

    Update paper.

--- java/uk/co/aldigital/ben/lucre/UnsignedCoin.java
+++ java/uk/co/aldigital/ben/lucre/UnsignedCoin.java
@@ -74,18 +74,22 @@ class UnsignedCoin {
 	int nCoinLength=(m_biCoinID.bitLength()+7)/8;
 	int nDigestIterations=(bank.getPrimeLength()-nCoinLength)
 	  /PublicBank.DIGEST_LENGTH;
+	int n;
 
 	// 1 in 256 will be 1 byte shorter...
 	if(nCoinLength > bank.getCoinLength())
 	    return null;
 
 	byte xplusd[]=new byte[bank.getPrimeLength()];
+	for(n=0 ; n < bank.getCoinLength()-nCoinLength ; ++n)
+	    xplusd[n]=0;
 
-	Util.byteCopy(xplusd,0,m_biCoinID.toByteArray(),0,nCoinLength);
+	Util.byteCopy(xplusd,n,m_biCoinID.toByteArray(),0,nCoinLength);
+	nCoinLength+=n;
 
 	//	Util.addCrypto();
 	MessageDigest sha1=MessageDigest.getInstance("SHA-1");
-	for(int n=0 ; n < nDigestIterations ; ++n) {
+	for(n=0 ; n < nDigestIterations ; ++n) {
 	    sha1.update(xplusd,0,nCoinLength+PublicBank.DIGEST_LENGTH*n);
 	    Util.byteCopy(xplusd,nCoinLength+PublicBank.DIGEST_LENGTH*n,
 			  sha1.digest(),0,PublicBank.DIGEST_LENGTH);
