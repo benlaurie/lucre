03ac96f7631cf344ca3a242869f967426a56b7c3
Ben Laurie <ben@algroup.co.uk>
2003-06-02 11:58:34 +0000

    Add version number.

--- java/uk/co/aldigital/ben/lucre/Implementation.java
+++ java/uk/co/aldigital/ben/lucre/Implementation.java
@@ -1,5 +1,5 @@
 /* ====================================================================
- * Copyright (c) 1999, 2000 Ben Laurie.  All rights reserved.
+ * Copyright (c) 1999, 2000, 2003 Ben Laurie.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -295,6 +295,15 @@ class Implementation {
 	}
     }
 
+    static void doVersion(String args[]) {
+        if(args.length != 1)
+	    {
+	    System.err.println("version");
+	    System.exit(1);
+	    }
+	System.out.println(Version.VERSION);
+    }
+
     public static void main(String args[])
       throws IOException,NoSuchAlgorithmException {
 	if(args.length < 1) {
@@ -328,6 +337,8 @@ class Implementation {
 	    doZK1Respond(args);
 	} else if(function.equals("zk1-verify")) {
 	    doZK1Verify(args);
+	} else if(function.equals("version")) {
+	    doVersion(args);
 	} else {
 	    System.err.println("Unknown function: "+function);
 	    System.exit(2);
